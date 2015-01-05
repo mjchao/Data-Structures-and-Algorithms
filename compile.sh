ddf1cd21341ef2dcdbff4afe565e84ab3eab38fa
Mickey Chao <mjchao2@gmail.com>
2015-01-05 17:21:02 -0500

    Delete compile.sh
    Renamed to run.sh

--- compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++0x main.cpp UnitTest.cpp ArrayListTests.cpp LinkedListTests.cpp LinkedStackTests.cpp LinkedQueueTests.cpp ArrayQueueTests.cpp HashMapTests.cpp PriorityQueueTests.cpp BPTreeTests.cpp
