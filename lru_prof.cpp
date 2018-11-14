ee9d3c1027d66c4293c5482ce55c8d65e8ed4c22
Mickey Chao <mjchao2@gmail.com>
2018-11-13 21:24:10 -0600

    Profile get/use lru.

--- lru_prof.cpp
+++ lru_prof.cpp
@@ -63,6 +63,55 @@ void ProfileGetAndUseFront(int num_elems, int num_runs) {
   PrintStats(stop - start, num_elems * num_runs, "\t");
 }
 
+
+void ProfileGetAndUseBack(int num_elems, int num_runs) {
+  // doesn't work if number of elements less than 3
+  assert(num_elems > 3);
+  std::vector<int64_t> elems;
+  std::vector<int64_t> elem_to_mark_used;
+  for (int i = 0; i < num_elems; ++i) {
+    elems.push_back(i);
+
+    // Precompute this outside of profiling loop because this alternating
+    // branching could have significant time penalty.
+    if (i % 2 == 0) {
+      elem_to_mark_used.push_back(num_elems - 2);
+    } else {
+      elem_to_mark_used.push_back(num_elems - 1);
+    }
+  }
+
+  LruQueue<int64_t> test(elems);
+  int64_t start = Clock::Now();
+  for (int i = 0; i < num_runs; ++i) {
+    for (int j = 0; j < num_elems; ++j) {
+      test.MarkUsed(elem_to_mark_used[j]);
+    }
+  }
+  int64_t stop = Clock::Now();
+  std::cout << "dsalgo lru" << std::endl;
+  PrintStats(stop - start, num_elems * num_runs, "\t");
+}
+
+
+void ProfileGetAndUseRandom(int num_elems, int num_runs) {
+  std::vector<int64_t> elems;
+  for (int i = 0; i < num_elems; ++i) {
+    elems.push_back(i);
+  }
+  std::vector<int> elem_to_mark_used = RandN(0, num_elems - 1, num_runs);
+
+  LruQueue<int64_t> test(elems);
+  int64_t start = Clock::Now();
+  for (int i = 0; i < num_runs; ++i) {
+    test.MarkUsed(elem_to_mark_used[i]);
+  }
+  int64_t stop = Clock::Now();
+  std::cout << "dsalog lru" << std::endl;
+  PrintStats(stop - start, num_runs, "\t");
+}
+
+
 void ProfileGetAndUse() {
 
   std::cout << "=== Profile small Lru get + use front ===" << std::endl;
@@ -75,6 +124,30 @@ void ProfileGetAndUse() {
 
   std::cout << "=== Profile large Lru get + use front ===" << std::endl;
   ProfileGetAndUseFront(1000000, 1);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profile small Lru use back ===" << std::endl;
+  ProfileGetAndUseBack(10, 100000);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profile medium Lru use back ===" << std::endl;
+  ProfileGetAndUseBack(1000, 1000);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profile medium Lru use back ===" << std::endl;
+  ProfileGetAndUseBack(1000000, 1);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profile small lru use random ===" << std::endl;
+  ProfileGetAndUseRandom(10, 100000);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profile medium lru use random ===" << std::endl;
+  ProfileGetAndUseRandom(1000, 100000);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profile large lru use random ===" << std::endl;
+  ProfileGetAndUseRandom(1000000, 100000);
   std::cout << "\n\n\n";
 }
 
