1885b116853c442db9a958df5345aec66a85b878
Mickey Chao <mjchao2@gmail.com>
2018-11-24 03:39:21 +0000

    Profile deque push/pop front.

--- deque_prof.cpp
+++ deque_prof.cpp
@@ -7,7 +7,7 @@
 using namespace dsalgo;
 
 
-void ProfilePushBack(int deque_size, int num_runs) {
+void ProfilePushPopBack(int deque_size, int num_runs) {
   std::vector<int> rand_elems = RandN(-100000, 100000, deque_size); 
 
   int64_t start = 0;
@@ -19,7 +19,9 @@ void ProfilePushBack(int deque_size, int num_runs) {
     for (int j = 0; j < deque_size; ++j) {
       test.PushBack(rand_elems[j]);
     }
-    test.Clear();
+    for (int j = 0; j < deque_size; ++j) {
+      test.PopBack();
+    }
   }
   stop = Clock::Now();
 
@@ -32,7 +34,9 @@ void ProfilePushBack(int deque_size, int num_runs) {
     for (int j = 0; j < deque_size; ++j) {
       test_std.push_back(rand_elems[j]);
     }
-    test_std.clear();
+    for (int j = 0; j < deque_size; ++j) {
+      test_std.pop_back();
+    }
   }
   stop = Clock::Now();
   std::cout << "std::deque" << std::endl;
@@ -40,17 +44,17 @@ void ProfilePushBack(int deque_size, int num_runs) {
 }
 
 
-void ProfilePushBackVariousSizes() {
+void ProfilePushPopBackVariousSizes() {
   std::cout << "=== Profiling Deque Push Back Small Size ===" << std::endl;
-  ProfilePushBack(10, 100000);
+  ProfilePushPopBack(10, 100000);
   std::cout << "\n\n\n";
 
   std::cout << "=== Profiling Deque Push Back Medium Size ===" << std::endl;
-  ProfilePushBack(100, 10000);
+  ProfilePushPopBack(100, 10000);
   std::cout << "\n\n\n";
 
   std::cout << "=== Profiling Deque Push Back Large Size ===" << std::endl;
-  ProfilePushBack(10000, 100);
+  ProfilePushPopBack(10000, 100);
   std::cout << "\n\n\n";
 }
 
@@ -116,10 +120,64 @@ void ProfileAccessVariousSizes() {
   std::cout << "\n\n\n";
 }
 
+
+void ProfilePushPopFront(int deque_size, int num_runs) {
+  std::vector<int> rand_elems = RandN(-100000, 100000, deque_size); 
+
+  int64_t start = 0;
+  int64_t stop = 0;
+  
+  Deque<int> test;
+  start = Clock::Now();
+  for (int i = 0; i < num_runs; ++i) {
+    for (int j = 0; j < deque_size; ++j) {
+      test.PushFront(rand_elems[j]);
+    }
+    for (int j = 0; j < deque_size; ++j) {
+      test.PopFront();
+    }
+  }
+  stop = Clock::Now();
+
+  std::cout << "dsalgo Deque" << std::endl;
+  PrintStats(stop - start, num_runs * deque_size, "\t");
+
+  std::deque<int> test_std;
+  start = Clock::Now();
+  for (int i = 0; i < num_runs; ++i) {
+    for (int j = 0; j < deque_size; ++j) {
+      test_std.push_front(rand_elems[j]);
+    }
+    for (int j = 0; j < deque_size; ++j) {
+      test_std.pop_front();
+    }
+  }
+  stop = Clock::Now();
+  std::cout << "std::deque" << std::endl;
+  PrintStats(stop - start, num_runs * deque_size, "\t");
+}
+
+
+void ProfilePushPopFrontVariousSizes() {
+  std::cout << "=== Profiling Deque Push/Pop Front Small Size ===" << std::endl;
+  ProfilePushPopFront(10, 100000);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profiling Deque Push/Pop Front Medium Size ===" << std::endl;
+  ProfilePushPopFront(100, 10000);
+  std::cout << "\n\n\n";
+
+  std::cout << "=== Profiling Deque Push/Pop Front Large Size ===" << std::endl;
+  ProfilePushPopFront(10000, 100);
+  std::cout << "\n\n\n";
+}
+
+
 int main() {
   ReseedRand();
-  ProfilePushBackVariousSizes();
+  ProfilePushPopBackVariousSizes();
   ProfileAccessVariousSizes();
+  ProfilePushPopFrontVariousSizes();
   return 0;
 }
 
